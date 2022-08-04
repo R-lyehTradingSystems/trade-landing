befc1ee1c9995c964f120229b2661c18f0fad3d6
DanilR <aringai09@gmail.com>
2022-08-04 17:47:39 +0300

    add: Logo

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,23 @@ export default {
 </script>
 
 <style>
+:root {
+  --main-background: #fff;
+  --cta-color: #00a985;
+  --secondary-color: #fa7964;
+  --color: #333;
+}
+
 html {
   scroll-behavior: smooth;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #f5a425;
-  background-color: #1b273e;
+  color: var(--secondary-color);
+  background-color: var(--main-background);
 }
 </style>
